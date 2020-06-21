e19c8369479594d372772857b39ff9f8c6fa2735
donasuris <63824718+donasuris@users.noreply.github.com>
2020-06-21 15:58:23 +1000

    new tasks

---
+++ №112634. Связанность/№112634. Связанность/№112634. Связанность.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector<vector<int>> arr(N, vector<int>(N));
+	vector<bool> check(N);
+	check[0] = true;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) cin >> arr[i][j];
+	}
+
+	for (int x = 0; x < 2; x++) {
+		for (int k = 0; k < N; k++) {
+			for (int i = 0, j = k; i < N; i++) if (check[i] && arr[i][j]) check[i] = true; //для строк
+			for (int i = k, j = 0; j < N; j++) if (check[i] && arr[i][j]) check[j] = true; //для столбцов
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+		if (!check[i]) {
+			cout << "NO";
+			return 0;
+		}
+	cout << "YES";
+}
+
