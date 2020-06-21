4c966725eeb35e07cd11c03d52ca05d75d415822
donasuris <63824718+donasuris@users.noreply.github.com>
2020-06-21 23:50:50 +1000

    new tasks

---
+++ №112639. Самый дешёвый тур/№112639. Самый дешёвый тур/№112639. Самый дешёвый тур.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+int arr[9][9];
+int way[9];
+int best_way[9];
+int n, k, best;
+bool mas[9];
+
+void cycles(int start, int cur, int hop) {
+	if (hop == 1) {
+		if (arr[cur][start] != 0) {
+			way[k] = start;
+			int prev = start, len = 0;
+			for (int i = 1; i <= k; len += arr[prev][way[i]], prev = way[i], i++);
+			if (len < best) {
+				best = len;
+				for (int i = 0; i <= k; i++)
+					best_way[i] = way[i];
+			}
+		}
+		return;
+	}
+	mas[cur] = true;
+	for (int i = start + 1; i <= n; i++)
+		if (!mas[i] && arr[cur][i] != 0) {
+			way[k - hop + 1] = i;
+			cycles(start, i, hop - 1);
+		}
+	mas[cur] = false;
+}
+
+int main()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			cin >> arr[i][j];
+	cin >> k;
+	best = INT32_MAX;
+
+	for (int i = 1; i <= n; i++) {
+		way[0] = i;
+		cycles(i, i, k);
+	}
+
+	if (best == INT32_MAX) 
+		cout << 0 << endl;
+	else {
+		cout << best << endl;
+		for (int i = 0; i <= k; i++)
+			cout << best_way[i] << " ";
+		cout << endl;
+	}
+}
---
+++ №112650. Электрификация/№112650. Электрификация/№112650. Электрификация.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void sort(vector<pair<int, int>>& mas) {
+	int n = mas.size();
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (mas[i].first < mas[j].first || (mas[i].first == mas[j].first && mas[i].second < mas[j].second))
+				swap(mas[i], mas[j]);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<pair<int, int>> arr(n - 1);
+	int** map = new int*[n];
+	int* color = new int[n];
+	for (int i = 0; i < n; i++) {
+		if (i > 0) arr[i - 1] = make_pair(-1, -1);
+		color[i] = i;
+		map[i] = new int[n];
+		for (int j = 0; j < n; j++) cin >> map[i][j];
+	}
+
+	int x, i_col, j_col;
+	for (int k = 0; k < n - 1; k++) {
+		x = INT32_MAX;
+		for (int i = 0; i < n; i++)
+			for (int j = i + 1; j < n; j++)
+				if (map[i][j] != 0 && color[i] != color[j] && map[i][j] < x) {
+					x = map[i][j];
+
+					arr[k].first = i;
+					arr[k].second = j;
+
+					i_col = color[i];
+					j_col = color[j];
+				}
+		if (arr[k].first > arr[k].second)
+			swap(arr[k].first, arr[k].second);
+		for (int i = 0; i < n; i++)
+			if (color[i] == j_col) color[i] = i_col;
+	}
+
+	sort(arr);
+	for (int i = 0; i < n - 1; i++)
+		cout << arr[i].first + 1 << ' ' << arr[i].second + 1 << endl;
+}
